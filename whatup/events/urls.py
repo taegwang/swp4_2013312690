9649cf6195f4f2b5f52bc9ad73626c54a98ebfee
pjh <whdgur262@naver.com>
2015-11-11 11:50:25 -0800

    lecture 5 complete

--- whatup/events/urls.py
+++ whatup/events/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from events import views
 
 urlpatterns = [
+	url(r'^create/$', views.create, name='ev_create'),
 	url(r'^tonight/$', views.tonight, name='ev_tonight'),
 ]
--- whatup/events/views.py
+++ whatup/events/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response
 from events.models import Event
 from django.template import RequestContext
+from events.forms import EventForm
+from dateutil.parser import parse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 # Create your views here.
 def tonight(request):
@@ -12,4 +17,30 @@ def tonight(request):
 		'events/tonight.html',
 		context,
 		context_instance=RequestContext(request),
+	)
+
+def create(request):
+	form = EventForm(request.POST or None)
+	if form.is_valid():
+		event = form.save(commit=False)
+		event.creator = request.user
+		guessed_date = None
+		for word in event.description.split():
+			try:
+				guessed_date = parse(word)
+				break
+			except ValueError:
+				continue
+		event.start_date = guessed_date
+		event.save()
+		messages.success(request, "Your event was posted")
+		if 'next' in request.POST:
+			next = request.POST['next']
+		else:
+			next = reverse('ev_tonight')
+		return HttpResponseRedirect(next)
+	return render_to_response(
+			'events/create.html',
+			{'form': form},
+			context_instance = RequestContext(request)
 		)
